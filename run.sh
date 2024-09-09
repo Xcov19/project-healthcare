953e5dece0ca489367fbf24a729c2541730606bd
codecakes <codecakes@users.noreply.github.com>
2024-09-09 16:27:58 +0530

    Feature/sqlite repo:  Identify type signature and annotations for interface and sqlite implementation (#66)
    Prepares testing support for #26 
    
    - adds support for test markers
    - `make test` works. added test-integration marker and is currently WIP.
    - refactored unit tests to have dummy functions move to conftest. fix
    their type annotaitons. added e2e api testing support in start_server
    and conftest. added unit test configuration setup in pyproject.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-poetry run python3 -m xcov19.dev
+APP_ENV=dev APP_DB_ENGINE_URL="sqlite+aiosqlite:///xcov19.db" poetry run python3 -m xcov19.dev
--- xcov19/app/settings.py
+++ xcov19/app/settings.py
@@ -7,8 +7,9 @@ values.
 https://docs.pydantic.dev/latest/usage/settings/
 """
 
+from typing import Annotated
 from blacksheep import FromHeader
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -26,6 +27,8 @@ class Site(BaseModel):
 
 
 class Settings(BaseSettings):
+    db_engine_url: Annotated[str, "database connection string"] = Field(default=...)
+
     # to override info:
     # export app_info='{"title": "x", "version": "0.0.2"}'
     info: APIInfo = APIInfo()
@@ -34,13 +37,14 @@ class Settings(BaseSettings):
     # export app_app='{"show_error_details": True}'
     app: App = App()
 
-    db_engine_url: str = "sqlite+aiosqlite:///"  # "sqlite+aiosqlite:///xcov19.db"
-
     model_config = SettingsConfigDict(env_prefix="APP_")
 
 
 def load_settings() -> Settings:
-    return Settings()
+    settings = Settings()
+    if not settings.db_engine_url:
+        raise ValueError("Missing environment variable: APP_DB_ENGINE_URL")
+    return settings
 
 
 class FromOriginMatchHeader(FromHeader[str]):
