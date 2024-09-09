c8f23f9469c9ce1005d1597667b66873131fef02
codecakes <akulmat@protonmail.com>
2024-09-10 03:24:40 +0530

    major rewrite of test services for integration test for Sqlite Repo DB. refactored code to use container to setup database using start_test_database and asyncSetUp. WIP: test_fetch_facilities

---
+++ xcov19/tests/data/seed_db.py
@@ -0,0 +1,43 @@
+"""Dummy data to seed to database models.
+Mapped to SQLModel.
+
+dummy GeoLocation:
+lat=0
+lng=0
+
+cust_id=test_cust_id
+query_id=test_query_id
+"""
+
+from sqlalchemy import ScalarResult
+from sqlmodel import select
+from xcov19.infra.models import Patient, Query, Location
+from sqlmodel.ext.asyncio.session import AsyncSession as AsyncSessionWrapper
+
+
+async def seed_data(session: AsyncSessionWrapper):
+    """
+    Now you can do:
+    res = await self._session.exec(select(Query))
+    query = res.first()
+    print("query", query)
+    res = await self._session.exec(select(Patient).where(Patient.queries.any(Query.query_id == query.query_id)))
+    print("patient", res.first())
+    res = await self._session.exec(select(Location).where(Location.queries.any(Query.query_id == query.query_id)))
+    print("location", res.first())
+    """
+    query = Query(
+        query="""
+        Runny nose and high fever suddenly lasting for few hours.
+        Started yesterday.
+        """
+    )  # type: ignore
+
+    patient = Patient(queries=[query])  # type: ignore
+
+    patient_location = Location(latitude=0, longitude=0, queries=[query])  # type: ignore
+    session.add_all([patient_location, patient])
+    await session.commit()
+    query_result: ScalarResult = await session.exec(select(Query))
+    if not query_result.first():
+        raise RuntimeError("Database seeding failed")
